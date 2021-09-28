2b98620112eb97900ccc5ac46a33c95691cba5c4
Loitp <v.loitp@cloudhms.net>
2021-09-28 20:34:15 +0700

    update stream

--- samplebroadcast/src/main/java/com/uiza/UZApplication.kt
+++ samplebroadcast/src/main/java/com/uiza/UZApplication.kt
@@ -7,7 +7,7 @@ class UZApplication : MultiDexApplication() {
 
     companion object {
         const val URL_STREAM =
-            "rtmp://a.rtmp.youtube.com/live2/8bdq-a4kv-q0k5-p2xm-2hqx"
+            "rtmp://rtmp.gcpsg.uizadev.io/live/live_JwkMLERERP"
     }
 
     override fun onCreate() {
